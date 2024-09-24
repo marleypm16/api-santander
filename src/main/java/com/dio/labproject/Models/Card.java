01b7adf67e227f11abd2cb0123bcfa393a600e31
marleypm16 <yarlempm16@gmail.com>
2024-09-24 10:16:19 -0300

    feat : add card modal

---
+++ src/main/java/com/dio/labproject/Models/Card.java
@@ -0,0 +1,26 @@
+package com.dio.labproject.Models;
+
+import java.math.BigDecimal;
+
+public class Card {
+    private String number;
+    private BigDecimal limit;
+
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Number getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
