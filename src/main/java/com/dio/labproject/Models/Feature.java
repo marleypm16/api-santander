43a81b7e928f119e548e8079f4d4eb1a305d41e8
marleypm16 <yarlempm16@gmail.com>
2024-09-24 10:16:28 -0300

    feat : add feature modal

---
+++ src/main/java/com/dio/labproject/Models/Feature.java
@@ -0,0 +1,22 @@
+package com.dio.labproject.Models;
+
+public class Feature {
+    private String icon;
+    private String description;
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
