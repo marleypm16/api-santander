cf4e50e9b4577d74d9b9bf42a7cf6fd996a881b6
marleypm16 <yarlempm16@gmail.com>
2024-09-24 09:49:05 -0300

    chore : initial commit

---
+++ src/main/java/com/dio/labproject/LabprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dio.labproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/labproject/LabprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.labproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
